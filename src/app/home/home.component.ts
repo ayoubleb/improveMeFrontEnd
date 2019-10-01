2faf3dd574b8e7fe9ee8208019727f6088c75784
Ayoubleb <lebhal@gmail.com>
2019-10-01 15:57:24 +0100

    story observable added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { TokenStorageService } from '../auth/token-storage.service';
+import {StoryService} from '../services/story.service';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +10,9 @@ import { TokenStorageService } from '../auth/token-storage.service';
 })
 export class HomeComponent implements OnInit {
   info: any;
+  stories: any;
 
-  constructor(private token: TokenStorageService) { }
+  constructor(private token: TokenStorageService, private storyservice: StoryService) { }
 
   ngOnInit() {
     this.info = {
@@ -18,6 +20,18 @@ export class HomeComponent implements OnInit {
       username: this.token.getUsername(),
       authorities: this.token.getAuthorities()
     };
+
+    this.storyservice.getAllStories().subscribe(
+      data => {
+        console.log(data);
+        this.stories = data;
+      },
+      error => {
+        console.log(error);
+       // this.errorMessage = error.error.message;
+       // this.isSignUpFailed = true;
+      }
+    );
   }
 
   logout() {
--- src/app/services/story.service.ts
+++ src/app/services/story.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Story} from '../model/story';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoryService {
 
-  constructor() { }
+  private storyUrl = 'http://localhost:8080/api/story';
+
+  constructor(private http: HttpClient) { }
+
+  getAllStories(): Observable<Story[]> {
+    return this.http.get<Story[]>(this.storyUrl + '/all');
+  }
 }
