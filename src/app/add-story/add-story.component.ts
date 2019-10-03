640896681261b752f5e61172e503e5955782e7a2
Ayoubleb <lebhal@gmail.com>
2019-10-03 14:27:52 +0100

    add story 1

--- src/app/add-story/add-story.component.ts
+++ src/app/add-story/add-story.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {SignUpInfo} from '../auth/signup-info';
+import {StoryInfo} from './story-info';
 
 @Component({
   selector: 'app-add-story',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-story.component.css']
 })
 export class AddStoryComponent implements OnInit {
-
+  storyInfo: StoryInfo;
   constructor() { }
 
   ngOnInit() {
   }
+onSubmit()
+  this.storyInfo = new StoryInfo(
+    this.form.name,
+    this.form.username,
+    this.form.email,
+    this.form.password);
+
+  this.authService.signUp(this.signupInfo).subscribe(
+    data => {
+  console.log(data);
+  this.isSignedUp = true;
+  this.isSignUpFailed = false;
+},
+error => {
+  console.log(error);
+  this.errorMessage = error.error.message;
+  this.isSignUpFailed = true;
+}
+);
 
 }
